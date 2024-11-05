2ef7b32d769475243bfe8ebdb0d761309e367c77
andrei.pugatsov <andrei.pugatsov@gmail.com>
2024-11-05 15:09:51 +0200

    handleValidationExceptions added to Custom Exception

--- src/main/java/com/example/sectors_app/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/sectors_app/exception/CustomExceptionHandler.java
@@ -4,9 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -17,6 +22,20 @@ public class CustomExceptionHandler {
     public final ResponseEntity<String> handleCustomException(CustomException ex, WebRequest request) {
         logger.error("CustomException: " + ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), ex.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            fieldErrors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("errors", fieldErrors);
+
+        return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
