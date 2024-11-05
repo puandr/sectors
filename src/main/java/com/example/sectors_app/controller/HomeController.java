aecc908888f38c39606b878a9b58200bcc784890
andrei.pugatsov <andrei.pugatsov@gmail.com>
2024-11-05 10:44:19 +0200

    Minor refactor

--- src/main/java/com/example/sectors_app/controller/HomeController.java
+++ src/main/java/com/example/sectors_app/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    // TODO test with React build in static folder
     @GetMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/";
