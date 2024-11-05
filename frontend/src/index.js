b342a5fdee81d9a6582a7dea4dc8e650e43736c7
andrei.pugatsov <andrei.pugatsov@gmail.com>
2024-11-05 18:22:22 +0200

    Frontend: dividing to smaller components, implementing tests, some css, part 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SectorsForm from './components/SectorFrom'; 
+import FormContainer from './components/FormContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <SectorsForm />
+      <FormContainer />
     </React.StrictMode>
   );
